ca0d1c87e0932624a6fd498cf995f7e9a5d51939
777 <jaehyun777@gmail.com>
2023-03-17 04:25:14 -0700

    alpabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - prints appahbet
+  *
+  * Return: lowercase
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
