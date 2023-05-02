d171b7a381046556fa0824998d3aeaa5b4e4e8e9
jaehyun <tammy@gmail.com>
2023-05-02 09:44:09 -0700

    udd

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t amount = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		amount++;
 		h = h->next;
 	}
 
-	return (count);
+	return (amount);
 }
